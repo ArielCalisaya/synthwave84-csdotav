bc58544a8dcaa994db263702c66a786f21ed7114
CSAV <calisayaariel62@gmail.com>
2021-04-08 02:25:31 -0400

    added monokai syntax colors

---
+++ themes/test.js
@@ -0,0 +1,8 @@
+const holamundo= "hola"
+
+const GeekForce = (props) => {
+  const {data, pageContext, yml} = props;
+  const partnersData = data.allPartnerYaml.edges[0].node;
+  const content = data.allPageYaml.edges[0].node
+  var contente = "hello"
+}
